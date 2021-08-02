8946c1ea8ca2aa86ac28d3fbb6684c8d9be48ef7
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-08-02 15:22:55 +0300

    Part4 tests implemented

--- src/main/java/com/epam/rd/java/basic/practice6/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice6/Demo.java
@@ -4,23 +4,29 @@ import com.epam.rd.java.basic.practice6.part1.Part1;
 import com.epam.rd.java.basic.practice6.part2.Part2;
 import com.epam.rd.java.basic.practice6.part3.Part3;
 import com.epam.rd.java.basic.practice6.part4.Part4;
+import com.epam.rd.java.basic.practice6.part5.Part5;
 
 public class Demo {
 
+	private static final String SEPARATOR = "--------------------";
+
 	public static void main(String[] args) {
-		String separator = "--------------------";
 		System.out.println("Part 1 demo:");
 		Part1.main(args);
-		System.out.println(separator);
-		System.out.println("Part 2 demo:");
+		printHeader(2);
 		Part2.main(args);
-		System.out.println(separator);
-		System.out.println("Part 3 demo:");
+		printHeader(3);
 		Part3.main(args);
-		System.out.println(separator);
-		System.out.println("Part 4 demo:");
+		printHeader(4);
 		Part4.main(args);
-		System.out.println(separator);
+		printHeader(5);
+		Part5.main(args);
+		System.out.println(SEPARATOR);
+	}
+
+	private static void printHeader(int n) {
+		System.out.println(SEPARATOR);
+		System.out.printf("Part %d demo:", n);
 	}
 
 }
--- src/test/java/com/epam/rd/java/basic/practice6/part4/RangeTest.java
+++ src/test/java/com/epam/rd/java/basic/practice6/part4/RangeTest.java
@@ -1,5 +1,33 @@
 package com.epam.rd.java.basic.practice6.part4;
 
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+
 public class RangeTest {
+
+    private final Range direct = new Range(3, 10);
+    private final Range reverse = new Range(3, 10, true);
+
+    @Test
+    public void lastReturnedValueShouldBe10() {
+        Iterator<Integer> i = direct.iterator();
+        int n = 0;
+        while (i.hasNext()) {
+            n = i.next();
+        }
+        assertEquals(10, n);
+    }
+
+    @Test
+    public void reverseShouldReturn109876543() {
+        StringBuilder sb = new StringBuilder();
+        for (int n : reverse) {
+            sb.append(n);
+        }
+        assertEquals("109876543", sb.toString());
+    }
 
 }
