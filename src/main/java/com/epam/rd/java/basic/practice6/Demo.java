933bfb988b033c211717142238064858c682710f
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-08-02 14:44:18 +0300

    Part3 fixed

--- src/main/java/com/epam/rd/java/basic/practice6/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice6/Demo.java
@@ -7,15 +7,19 @@ import com.epam.rd.java.basic.practice6.part3.Part3;
 public class Demo {
 
 	public static void main(String[] args) {
+		String separator = "--------------------";
 		System.out.println("Part 1 demo:");
 		Part1.main(args);
-		System.out.println("--------------------");
+		System.out.println(separator);
 		System.out.println("Part 2 demo:");
 		Part2.main(args);
-		System.out.println("--------------------");
+		System.out.println(separator);
 		System.out.println("Part 3 demo:");
 		Part3.main(args);
-		System.out.println("--------------------");
+		System.out.println(separator);
+		System.out.println("Part 4 demo:");
+		Part3.main(args);
+		System.out.println(separator);
 	}
 
 }
--- src/main/java/com/epam/rd/java/basic/practice6/part3/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part3/Part3.java
@@ -4,41 +4,30 @@ public class Part3 {
 
     public static void main(String[] args) {
         Parking p = new Parking(4);
+        printArrive(p, 2);
+        printArrive(p, 3);
+        printArrive(p, 2);
+        printArrive(p, 2);
+        printArrive(p, 2);
+        printDepart(p, 1);
+        printArrive(p, 1);
+    }
+
+    private static void printArrive(Parking p, int i) {
         boolean b;
-        System.out.println("arrive(2)");
-        b = p.arrive(2); // 0010, true
-        p.print();
-        System.out.printf(", %b%n", b);
-
-        System.out.println("arrive(3)");
-        b = p.arrive(3); // 0011, true
-        p.print();
-        System.out.printf(", %b%n", b);
-
-        System.out.println("arrive(2)");
-        b = p.arrive(2); // 1011, true
-        p.print();
-        System.out.printf(", %b%n", b);
-
-        System.out.println("arrive(2)");
-        b = p.arrive(2); // 1111, true
-        p.print();
-        System.out.printf(", %b%n", b);
-
-        System.out.println("arrive(2)");
-        b = p.arrive(2); // 1111, false
-        p.print();
-        System.out.printf(", %b%n", b);
-
-        System.out.println("depart(1)");
-        b = p.depart(1); // 1011, true
-        p.print();
-        System.out.printf(", %b%n", b);
-
-        System.out.println("depart(1)");
-        b = p.depart(1); // 1011, false
+        System.out.printf("arrive(%d)%n", i);
+        b = p.arrive(i);
         p.print();
         System.out.printf(", %b%n", b);
     }
+
+    private static void printDepart(Parking p, int i) {
+        boolean b;
+        System.out.printf("depart(%d)%n", i);
+        b = p.depart(i);
+        p.print();
+        System.out.printf(", %b%n", b);
+    }
+
 
 }
