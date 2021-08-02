526ecf3ec5411cafac8006f16e11eff9cc021973
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-08-02 15:08:04 +0300

    Part4 implemented

--- src/main/java/com/epam/rd/java/basic/practice6/part4/Range.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part4/Range.java
@@ -1,34 +1,57 @@
 package com.epam.rd.java.basic.practice6.part4;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public class Range implements Iterable<Integer>{
+public class Range implements Iterable<Integer> {
+
+    private final int firstBound;
+    private final int secBound;
+    private final int step;
 
     public Range(int n, int m) {
         this(n, m, false);
     }
 
     public Range(int firstBound, int secBound, boolean reversedOrder) {
-        
+        if (firstBound >= secBound) {
+            throw new IllegalArgumentException();
+        }
+        if (reversedOrder) {
+            this.firstBound = secBound;
+            this.secBound = firstBound;
+            step = -1;
+        } else {
+            this.firstBound = firstBound;
+            this.secBound = secBound;
+            step = 1;
+        }
     }
-    
+
     @Override
     public Iterator<Integer> iterator() {
         return new IteratorImpl();
     }
-    
+
     private final class IteratorImpl implements Iterator<Integer> {
+
+        int index = firstBound;
 
         @Override
         public boolean hasNext() {
-            return false;
+            return (step == 1) ? index <= secBound : index >= secBound;
         }
 
         @Override
         public Integer next() {
-            return null;
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            int out = index;
+            index = index + step;
+            return out;
         }
-        
+
     }
 
 }
