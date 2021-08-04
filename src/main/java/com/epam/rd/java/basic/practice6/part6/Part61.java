8dc2a6f61c08f3ea3ffae807809b8dd8e2cbc4d2
Oleksii Kirpilov <kirpilev.aleksey@gmail.com>
2021-08-04 19:19:43 +0300

    fixes

--- src/main/java/com/epam/rd/java/basic/practice6/part6/Part61.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part6/Part61.java
@@ -1,21 +1,21 @@
 package com.epam.rd.java.basic.practice6.part6;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summingInt;
 
-public class Part61{
+public class Part61 {
 
-    private static final String ARROW = " ==> ";
-
-    private static Map<String, Integer> wordPositions;
-
-    private Part61() {}
+    private Part61() {
+    }
 
     public static void work(List<String> words) {
-        wordPositions = new HashMap<>();
+        Map<String, Integer> wordPositions = new HashMap<>();
         for (int i = 0; i < words.size(); i++) {
             String word = words.get(i);
             if (wordPositions.containsKey(word)) {
@@ -28,10 +28,9 @@ public class Part61{
                 .collect(groupingBy(Function.identity(), summingInt(e -> 1)));
         List<Map.Entry<String, Integer>> list =
                 wordsCount.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors.toList());
-
-        Collections.sort(list, (e1, e2) -> {
+                        .sorted(Map.Entry.comparingByValue())
+                        .collect(Collectors.toList());
+        list = list.stream().sorted((e1, e2) -> {
             int cmp = e2.getValue() - e1.getValue();
             if (cmp < 0) {
                 return -1;
@@ -41,14 +40,15 @@ public class Part61{
             }
             cmp = wordPositions.get(e1.getKey()) - wordPositions.get(e2.getKey());
             return (cmp < 1) ? -1 : 1;
-        });
+        }).limit(3).sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey()))
+                .collect(Collectors.toList());
 
         for (int i = 0; i < 3; i++) {
             String key = list.get(i).getKey();
-            Integer value = list.get(i).getValue();
-            System.out.println(key + ARROW + value);
+            int value = list.get(i).getValue();
+            System.out.printf("%s ==> %d%n", key, value);
         }
 
     }
-    
+
 }
